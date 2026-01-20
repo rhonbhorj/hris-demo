7f3a03db7a9135a3d20d79dc3b1639040e601eb9
michael angelo garcia <garciacruzmichaelangelo@gmail.com>
2026-01-20 14:01:39 +0800

    adjust css

--- application/views/dashboard/templates/tp_header.php
+++ application/views/dashboard/templates/tp_header.php
@@ -32,7 +32,7 @@
   <!-- Material Icons -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="/assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+  <link id="pagestyle" href="/assets/css/material-dashboard.css?v=3.2.1" rel="stylesheet" />
 </head>
 
 <body class="g-sidenav-show  bg-gray-100">
